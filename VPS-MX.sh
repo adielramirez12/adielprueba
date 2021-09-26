426c76d7fb6443a65fd8d4a874e71b2540ec3d96
adielramirez12 <91226124+adielramirez12@users.noreply.github.com>
2021-09-26 16:25:44 -0500

    Update VPS-MX.sh

--- VPS-MX.sh
+++ VPS-MX.sh
@@ -258,7 +258,7 @@ echo 'echo -e "\t\033[91m  \ \ / /| |_) \___ \ _____| |\/| |\  /  " '>> .bashrc
 echo 'echo -e "\t\033[91m   \ V / |  __/ ___) |_____| |  | |/  \  " '>> .bashrc
 echo 'echo -e "\t\033[91m    \_/  |_|   |____/      |_|  |_/_/\_\ " '>> .bashrc
 echo 'echo "" '>> .bashrc
-echo 'wget -O /etc/VPS-MX/banner1.txt https://raw.githubusercontent.com/adielramirez12/adielprueba/master/message.txt &> dev/null'>> .bashrc
+echo 'wget -O /etc/VPS-MX/message.txt https://raw.githubusercontent.com/adielramirez12/adielprueba/master/message.txt &> dev/null'>> .bashrc
 echo 'echo "" '>> .bashrc
 echo 'echo -e "\t\033[92mRESELLER : $mess1 "'>> .bashrc
 echo 'echo "" '>> .bashrc                                               
