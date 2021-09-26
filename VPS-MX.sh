c55aeb59b0e0bc48e1da251afd7383e2ff9ae45d
adielramirez12 <91226124+adielramirez12@users.noreply.github.com>
2021-09-26 00:27:13 -0500

    Update VPS-MX.sh

--- VPS-MX.sh
+++ VPS-MX.sh
@@ -373,7 +373,7 @@ msg -bar2
 [[ ${#1} -gt 2 ]] && funcao_idioma || id="$1"
  }
 Key="qraKatsil/33?030ce55?7dff/8888:%05+@%?+08?+91%@"
-REQUEST=$(echo $Adiel|$PUTO)
+REQUEST=$(echo $Kalix1|$PUTO)
 echo "$IP" > /usr/bin/vendor_code
 cd $HOME
 msg -ne "Files: "
